831ba488d0d697a19b3c475de6e18d7bb2b2902a
Bruno <bruno.f.viana@gmail.com>
2016-12-28 10:41:26 +0100

    New code added to manage different numbers

--- MultipleNumbers/Program.cs
+++ MultipleNumbers/Program.cs
@@ -16,15 +16,21 @@ namespace MultipleNumbers
             Console.WriteLine("Second word:");
             string secondWord = Console.ReadLine();
 
+            //New code to user select different numbers
+            Console.WriteLine("First Number:");
+            int firstNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Second number:");
+            int secondNumber = int.Parse(Console.ReadLine());
+
             //Cycle for to print the values from 1 to 100
             for (int i = 1; i <= 100; i++)
             {
                 //Logical conditions to know where to print a word or the numbers.
-                if ((i % 3 == 0) && (i % 5 != 0))
+                if ((i % firstNumber == 0) && (i % secondNumber != 0))
                     Console.WriteLine(firstWord);
-                else if ((i % 5 == 0) && (i % 3 != 0))
+                else if ((i % secondNumber == 0) && (i % firstNumber != 0))
                     Console.WriteLine(secondWord);
-                else if ((i % 3 == 0) && (i % 5 == 0))
+                else if ((i % firstNumber == 0) && (i % secondNumber == 0))
                     Console.WriteLine(firstWord+secondWord);
                 else
                     Console.WriteLine(i);
